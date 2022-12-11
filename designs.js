eb9e4c5444276c10de131be3cdd84021f3c7d3d5
Layoolar <46034532+Layoolar@users.noreply.github.com>
2022-12-11 23:28:18 +0100

    Updated design.js

--- designs.js
+++ designs.js
@@ -1,10 +1,66 @@
-// Select color input
-// Select size input
+// Target the submit button 
 
-// When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function clearBox(elementID) {
+    document.querySelector('#pixelCanvas').innerHTML = "";
+}
 
+function getColor() {
+const color = document.querySelector("#colorPicker").value;
+return color;
+}
+
+
+
+const formSubmit = document.querySelector('#submit');
+// Add an event listener to the submit button and prevent default
+// Get the values of height and width
+formSubmit.addEventListener('click', function (e) {
+    e.preventDefault();
+    const gridHeight = document.querySelector("#inputHeight").value;
+    const gridWidth = document.querySelector("#inputWidth").value;
+clearBox();
+makeGrid(gridHeight, gridWidth);
+
+})
+
+function makeGrid(gridHeight, gridWidth) {
+let thisTable = document.querySelector('#pixelCanvas');
+for (let i = 1; i <= gridHeight; i++) {
+    const newRow = document.createElement('tr');
+    for (let j = 1; j <= gridWidth; j++) {
+        const newCol = document.createElement('td');
+        newRow.appendChild(newCol);
+         newCol.addEventListener('click', respondToTheClick);
+}
+thisTable.appendChild(newRow);
+}
+
+
+
+// const newEl = document.createElement('tr');
+// const newRow = document.createElement('td');
+// newEl.appendChild(newRow);
+// thisTable.appendChild(newEl);
 // Your code goes here!
 
 }
+
+
+
+// function respondToTheClick(evt) {
+//     console.log('A paragraph was clicked: ' + evt.target.textContent);
+// }
+
+// for (let i = 1; i <= gridHeight; i++) {
+//     const newRow = document.createElement('tr');
+//     for (let j = 1; j <= gridWidth; j++) {
+//         const newCol = document.createElement('td');
+//         newRow.appendChild(newCol);
+// }
+// thisTable.appendChild(newRow);
+// }
+
+// document.body.appendChild(myCustomDiv);
+
+// myCustomDiv.addEventListener('click', respondToTheClick);
